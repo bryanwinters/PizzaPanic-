0dd8e0fad3a88a733b01463995d5e23d82bce49b
Bryan Winters <bryan.winters@hotmail.com>
2017-05-05 17:53:32 -0400

    Removing useless stuff
    [TASK] deleted easy save

---
+++ Assets/Scripts/Misc/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants : object {
+
+    //ENUMS
+    public enum Toppings {dough = 0, sauce = 1, cheese = 2, pepperoni = 3, bacon = 4, anchovies = 5, greenPepper = 6,
+        mushroom = 7, hotPepper = 8, pineapple = 9}
+    public enum Characters {meowzzarella = 0, octopie = 1, crustodile = 2, bonemando = 3}
+
+    //NUMBERS
+    public static int TEST_INT = 1;
+
+    //STRINGS
+    public const string TEST_STRING = "test";
+}
