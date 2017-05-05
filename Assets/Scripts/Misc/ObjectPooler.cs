96791ff5e194199d339d0587b1aecf1b87a9531a
Unknown <m.eaton.1988@gmail.com>
2017-05-05 19:40:34 -0400

    +added anthonys dough asset, doesnt work yet +added a scaled to 25 dough asset (not used) +added pepperoni prefab +added Mack Test scene +added object pooler (currently pools pepperonis) +added pizzatoppingunifier.cs, for adding toppings to the pizza parent +added toppingtossing.cs for throwing ingredients

---
+++ Assets/Scripts/Misc/ObjectPooler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour {
+
+    public static ObjectPooler SharedInstance;
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    public List<GameObject> PooledPepperoni;
+    //make lists of toppings to pool and do that
+
+    public GameObject objectToPool;
+    public int amountToPool;
+
+	// Use this for initialization
+	void Start () {
+        PooledPepperoni = new List<GameObject>();
+        for(int i = 0; i < amountToPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(objectToPool);
+            obj.SetActive(false);
+            PooledPepperoni.Add(obj);
+        }
+		
+	}
+	
+    public GameObject GetTopping(int toppingNumber)
+    {
+        if( toppingNumber == 0)
+        {
+            for (int i = 0; i < PooledPepperoni.Count; i++)
+            {
+                if (!PooledPepperoni[i].activeInHierarchy)
+                {
+                    return PooledPepperoni[i];
+                }
+            }
+        }
+
+        return null;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Misc/PizzaToppingUnifier.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PizzaToppingUnifier : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnCollisionEnter ( Collision c)
+    {
+        if( c.gameObject.layer == LayerMask.NameToLayer("Pizza"))
+        {
+            //become one;
+            gameObject.transform.parent = c.transform.root;
+            gameObject.layer = LayerMask.NameToLayer("PizzaTopping");
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            gameObject.GetComponent<Rigidbody>().useGravity = false;
+        }
+        else if( c.gameObject.layer == LayerMask.NameToLayer("PizzaTopping"))
+        {
+            //become one;
+            gameObject.transform.parent = c.transform.root;
+            gameObject.layer = LayerMask.NameToLayer("PizzaTopping");
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            gameObject.GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+}
