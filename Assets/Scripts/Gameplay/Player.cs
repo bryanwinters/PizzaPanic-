185fbdffe7bf30685ed94bd91f158fb2cfa96375
Bryan Winters <bryan.winters@hotmail.com>
2017-05-06 20:34:06 -0400

    HUD update
    [TASK] player associated with hud element
    [TASK] cycle toppings
    [TASK] pizza timer

--- Assets/Scripts/Gameplay/Player.cs
+++ Assets/Scripts/Gameplay/Player.cs
@@ -13,6 +13,12 @@ public class Player : MonoBehaviour {
     private HandMovement _handController;
     public HandMovement HandController { get { return _handController; } }
 
+    private string _controlsCycleRight;
+    private string _controlsCycleLeft;
+
+    private Constants.Toppings _activeTopping = Constants.Toppings.dough;
+    private PlayerHUD _hudRef;
+
     private void Awake () 
     {
         SetupVariables();
@@ -21,6 +27,8 @@ public class Player : MonoBehaviour {
     private void SetupVariables () 
     {
         _handController = this.GetComponent<HandMovement>();
+        _controlsCycleRight = Constants.CONTROLS_CYCLE_RIGHT + PlayerNumber.ToString();
+        _controlsCycleLeft = Constants.CONTROLS_CYCLE_LEFT + PlayerNumber.ToString();
     }
 
 	// Use this for initialization
@@ -32,5 +40,31 @@ public class Player : MonoBehaviour {
     public void Init ()
     {
         _handController.Init();
+
+        _hudRef = MenuManager.Instance.GetHudForPlayer(CharacterType);
+        if (_hudRef)
+            _hudRef.Init();
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        if (GameManager.Instance.GameState == Constants.GameState.game)
+        {
+            if (Input.GetButtonDown(_controlsCycleRight))
+            {
+                CycleToppings(1);
+            }
+
+            if (Input.GetButtonDown(_controlsCycleLeft))
+            {
+                CycleToppings(-1);
+            }
+        }
+    }
+
+    private void CycleToppings (int direction)
+    {
+        _activeTopping = _hudRef.CycleToppings(direction);
     }
 }
---
+++ Assets/Scripts/Gameplay/PlayerHUD.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHUD : MonoBehaviour {
+
+    public Constants.Characters CharacterType;
+
+    [SerializeField] private Image _activeTopping;
+    [SerializeField] private List<ToppingIcon> _toppingsList = new List<ToppingIcon>();
+
+    private int _currentTopping = 0;
+
+    public void Init ()
+    {
+        //get dough,cheese,sauce + 2 random and pull from MenuManager.Instance.Topping to populate data
+
+       
+    }
+
+    public Constants.Toppings CycleToppings (int dir)
+    {
+        _currentTopping += dir;
+
+        if (_currentTopping >= _toppingsList.Count)
+            _currentTopping = 0;
+
+        if (_currentTopping < 0)
+            _currentTopping = _toppingsList.Count - 1;
+
+        _activeTopping.transform.position = _toppingsList[_currentTopping].IconImage.transform.position;
+        return _toppingsList[_currentTopping].Topping;
+    }
+}
